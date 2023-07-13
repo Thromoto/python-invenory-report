7108fd7903d419041211a01fb6a18666943dac4c
Thiago Omoto <thi.omoto@gmail.com>
2023-07-13 19:38:05 -0300

    Teste o relatório individual gerado por Produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,23 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    produto = Product(
+        "1",
+        "curso",
+        "trybe",
+        "13/07/2023",
+        "14/07/2023",
+        "123456",
+        "xxxxxx",
+    )
+
+    assert str(produto) == (
+            f"The product {produto.id} - {produto.product_name} "
+            f"with serial number {produto.serial_number} "
+            f"manufactured on {produto.manufacturing_date} "
+            f"by the company {produto.company_name} "
+            f"valid until {produto.expiration_date} "
+            "must be stored according to the following instructions: "
+            f"{produto.storage_instructions}."
+    )
